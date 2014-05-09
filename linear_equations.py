1cd5d246435b2a3bf36a4f23c8c13fda118b3895
James Sully <sullyj3@gmail.com>
2014-05-09 14:15:41 +1000

    loads of haphazard shit. clean up later

--- linear_equations.py
+++ linear_equations.py
@@ -1,6 +1,11 @@
+#for now, this presumes that all endpoints are inclusive, rather than exclusive
+
 def solve(**kwargs):
     #y=mx+c
     y,m,x,c='y','m','x','c'
+    for key in kwargs:
+        kwargs[key]=float(kwargs[key])
+        
     if not len(kwargs)==3:
         raise TypeError("Need exactly 3 Arguments")
 
@@ -9,41 +14,76 @@ def solve(**kwargs):
             m,x,c=kwargs[m],kwargs[x],kwargs[c]
         except KeyError("wrong arguments"):
             pass
-        return float(m*x+c)
+        return m*x+c
 
     elif not 'm' in kwargs:
         try:
             y,x,c=kwargs[y],kwargs[x],kwargs[c]
         except KeyError("wrong arguments"):
             pass
-        return (y-c)/float(x)
+        return (y-c)/x
 
     elif not 'x' in kwargs:
         try:
             y,m,c=kwargs[y],kwargs[m],kwargs[c]
         except KeyError("wrong arguments"):
             pass
-        return (y-c)/float(m)
+        return (y-c)/m
             
     elif not 'c' in kwargs:
         try:
             y,m,x=kwargs[y],kwargs[m],kwargs[x]
         except KeyError("wrong arguments"):
             pass
-        return float(y-(m*x))
+        return y-(m*x)
 
-class line(object):
+class real_interval(object): #unfinished
+    def __init__(self,
+        #a vaid mathematical interval notation string, like (1,2).
+        #use [] on either or both sides for inclusive, () for exclusive
+        #leave one of the numbers blank for negative or positive infinity
+
+        parse_str='', 
+        left=None, #real number
+        right=None, #real number
+        left_is_inclusive=True, #bool
+        right_is_inclusive=True #bool
+        ):
+
+        if not parse_str=='':
+            assert (left==None and
+                    right==None and
+                    left_is_inclusive==True and
+                    right_is_inclusive==True)
+            
+            assert len(parse_str) in range(3,6)
+
+            if parse_str[0] == '(':
+                self.left_is_inclusive == False
+            elif parse_str[0] == '[':
+                self.left_is_inclusive == True
+            else:
+                raise ValueError("Error parsing real interval generation string {0} at position 0: Invalid Syntax".format(parse_str))
+
+class line(object): #unfinished
     def __init__(self,
             vertical=False,
             m=None,
             known_point_x=None,
             known_point_y=None,
             c=None,
-            endpoint_left=None,
-            endpoint_right=None):
+            endpoint_left=None, # x value
+            endpoint_right=None # x value
+            ):
+
+        #prevent integer division problems
+        for arg in (m,c,endpoint_left,endpoint_right):
+            if not arg is None:
+                arg=float(arg)
 
         self.vertical=vertical
         self.m=m
+        self.c=c
         self.endpoint_left=endpoint_left
         self.endpoint_right=endpoint_right
         
@@ -52,10 +92,14 @@ class line(object):
                 self.c=solve(m=m,x=known_point_x,y=known_point_y)
             if c and known_point_x>None and known_point_y>None:
                 self.m=solve(c=c,x=known_point_x,y=known_point_y)
+            if not endpoint_left is None:
+                self.y_endpoint_left = self.m * self.endpoint_left + self.c
+            if not endpoint_right is None:
+                self.y_endpoint_right = self.m * self.endpoint_right + self.c
 
-def furthest_left(line1,line2):
+def furthest_left(line1,line2): 
     if line1.endpoint_left == line2.endpoint_left: 
-        #covers equal numbers and both None
+        #covers 'endpoints are equal' and 'endpoints are both None'
         return None
     elif line1.endpoint_left is None and not line2.endpoint_left is None:
         return line1
@@ -67,7 +111,46 @@ def furthest_left(line1,line2):
         elif line2.endpoint_left < line1.endpoint_left: 
             return line2
 
+def furthest_down(line1,line2): #unfinished
+    if line1.endpoint_left == line2.endpoint_left: 
+        #covers 'endpoints are equal' and 'endpoints are both None'
+        return None
+    elif line1.endpoint_left is None and not line2.endpoint_left is None:
+        return line1
+    elif line2.endpoint_left is None and not line1.endpoint_left is None:
+        return line2
+    else: #both must have left endpoints, which are not equal
+        if line1.endpoint_left < line2.endpoint_left:
+            return line1
+        elif line2.endpoint_left < line1.endpoint_left: 
+            return line2
+
+def test_domain_overlap(leftmost, rightmost):
+    #sort input lines by furthest left
+    if furthest_left(leftmost,rightmost) is rightmost:
+        leftmost,rightmost=rightmost,leftmost
+    elif furthest_left(leftmost,rightmost) is None:
+        return True
+
+    if leftmost.endpoint_right > rightmost.endpoint_left:
+        return True
+    else:
+        return False
+
+def test_range_overlap(bottom, top): #unfinished
+    #sort input lines by furthest left
+    if furthest_left(leftmost,rightmost) is rightmost:
+        leftmost,rightmost=rightmost,leftmost
+    elif furthest_left(leftmost,rightmost) is None:
+        return True
+
+    if leftmost.endpoint_right > rightmost.endpoint_left:
+        return True
+    else:
+        return False
+
 def test_intersect(line1, line2): #unfinished
+
     #check if they both have domains
     #if they both do, see if they intersect.
     if not ((line1.endpoint_left is None and line1.endpoint_right is None) or
@@ -87,7 +170,7 @@ def test_intersect(line1, line2): #unfinished
         #figure out conditions where the domains intersect
         #right endpoint of line furthest left is RIGHT of left endpoint of other line
         if ((line1.endpoint_right >= line2.endpoint_left) or
-            (line2.endpoint_right >= line1.endpoint_left))
+        (line2.endpoint_right >= line1.endpoint_left)):
             pass
 
 
